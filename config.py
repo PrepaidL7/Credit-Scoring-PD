bbb6e2dc0977727387236450ca4d5ab28250da69
Diego Cardeña <124939096+PrepaidL7@users.noreply.github.com>
2025-06-10 21:11:44 -0600

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# config.py
-from pathlib import Path
-
-# Raw data path
-BASE_DIR = Path(r"C:/Users/SANTY/Documents/Portafolio/Project_04_Materials")
-RAW_DATA_PATH = BASE_DIR / "data" / "raw"
-PROCESSED_DATA_PATH = BASE_DIR / "data" / "processed"
